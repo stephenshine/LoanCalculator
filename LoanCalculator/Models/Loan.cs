c5cfe1de2ee595487203c33f869f5ce7250447a8
Stephen Shine <stephen.shine88@gmail.com>
2016-11-19 15:21:12 +0000

    Initial commit. Contains Loan model, home controller and index view

---
+++ LoanCalculator/Models/Loan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoanCalculator.Models
+{
+    public class Loan
+    {
+        public double loan { get; set; }
+        public double APR { get; set; }
+        public double termInMonths { get; set; }
+        public double MonthylyInterestRate { get { return (APR / 12) / 100; } }
+
+        public double monthlyRepayment { get { return this.CalculateRepayment(); } }
+
+        private double CalculateRepayment()
+        {
+            double payment = 0;
+            double power = Math.Pow((1 + MonthylyInterestRate), (termInMonths * -1));
+            double denominator = 1 - power;
+            double numerator = loan * (MonthylyInterestRate);
+            payment = numerator / denominator;
+
+            return payment;
+        }
+    }
+}
