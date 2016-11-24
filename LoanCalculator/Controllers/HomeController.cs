3e8877fa6fc76de38069b9f7d7a700467bc47232
Stephen Shine <stephen.shine88@gmail.com>
2016-11-24 21:05:44 +0000

    adding

--- LoanCalculator/Controllers/HomeController.cs
+++ LoanCalculator/Controllers/HomeController.cs
@@ -19,8 +19,10 @@ namespace LoanCalculator.Controllers
             return View();
         }
 
+        // Modelbinding extracts values from loan passed to method
         public PartialViewResult Result(decimal Amount = 1000, decimal APR = 5, int TermInMonths = 12)
         {
+            // LoanViewModel contains information to display about loan and chart
             LoanViewModel model = new LoanViewModel(new Loan(Amount, APR, TermInMonths));          
             return PartialView(model);
         }
