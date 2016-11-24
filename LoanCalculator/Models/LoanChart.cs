0d64f7a93187fc5315ee6cdf19c9c0df55192156
Stephen Shine <stephen.shine88@gmail.com>
2016-11-24 20:28:42 +0000

    splitting chart creation into own class and updating ViewModel class

---
+++ LoanCalculator/Models/LoanChart.cs
@@ -0,0 +1,38 @@
+﻿using Chart.Mvc.SimpleChart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoanCalculator.Models
+{
+    public class LoanChart
+    {
+        public LoanChart(Loan loan)
+        {
+            Loan = loan;
+        }
+
+        Loan Loan { get; set; }
+
+        public PieChart CreatePieChart()
+        {
+            PieChart pieChart = new PieChart();
+            SimpleData interest = new SimpleData()
+            {
+                Value = (double)Math.Round(Loan.TotalInterest, 2),
+                Label = "Interest",
+                Color = "rgba(220, 0, 0, 0.5)"
+            };
+            SimpleData capital = new SimpleData()
+            {
+                Value = (double)Math.Round(Loan.Amount, 2),
+                Label = "Capital",
+                Color = "rgba(0, 220, 0, 0.5)"
+            };
+            pieChart.Data.Add(interest);
+            pieChart.Data.Add(capital);
+            return pieChart;
+        }
+    }
+}
--- LoanCalculator/Models/LoanViewModel.cs
+++ LoanCalculator/Models/LoanViewModel.cs
@@ -9,39 +9,21 @@ namespace LoanCalculator.Models
 {
     public class LoanViewModel
     {
+        private Loan Loan { get; set; }
+        private LoanChart LoanChart { get; set; }
 
         public LoanViewModel(Loan loan)
         {
             Loan = loan;
+            LoanChart = new LoanChart(Loan);
         }
-        public Loan Loan { get; set; }
 
         public decimal Amount { get { return Loan.Amount; } }
         public decimal MonthlyRepayment { get { return Loan.MonthlyRepayment; } }
         public decimal TotalInterest { get { return Loan.TotalInterest; } }
         public decimal TotalRepaid { get { return Loan.TotalRepaid; } }
         public List<Transaction> Transactions { get { return Loan.Transactions; } }
+        public PieChart Pie { get { return LoanChart.CreatePieChart(); } }
 
-        public PieChart Pie { get { return CreatePieChart(); } }
-
-        private PieChart CreatePieChart()
-        {
-            PieChart pieChart = new PieChart();
-            SimpleData interest = new SimpleData()
-            {
-                Value = (double)Math.Round(TotalInterest, 2),
-                Label = "Interest",
-                Color = "rgba(220, 0, 0, 0.5)"
-            };
-            SimpleData capital = new SimpleData()
-            {
-                Value = (double)Math.Round(Amount, 2),
-                Label = "Capital",
-                Color = "rgba(0, 220, 0, 0.5)"
-            };
-            pieChart.Data.Add(interest);
-            pieChart.Data.Add(capital);
-            return pieChart;
-        }
     }
 }
