d2896f92b609f1ce950b09b089faeb9de51aa2cf
Stephen Shine <stephen.shine88@gmail.com>
2016-11-24 05:36:05 +0000

    Moving PieChart construction to LoanViewModel

--- LoanCalculator/Models/LoanViewModel.cs
+++ LoanCalculator/Models/LoanViewModel.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Chart.Mvc.ComplexChart;
+using Chart.Mvc.SimpleChart;
+using Chart.Mvc.Extensions;
 
 namespace LoanCalculator.Models
 {
@@ -14,11 +17,32 @@ namespace LoanCalculator.Models
         }
         public Loan Loan { get; set; }
 
+        public decimal Amount { get { return Loan.Amount; } }
         public decimal MonthlyRepayment { get { return Loan.CalculateMonthlyRepayment(); } }
         public decimal TotalInterest { get { return Loan.TotalInterest; } }
         public decimal TotalRepaid { get { return Loan.TotalRepaid; } }
-        public decimal Amount { get { return Loan.Amount; } }
         public List<Transaction> Transactions { get { return Loan.Transactions; } }
 
+        public PieChart Pie { get { return CreatePieChart(); } }
+
+        public PieChart CreatePieChart()
+        {
+            PieChart pieChart = new PieChart();
+            SimpleData interest = new SimpleData()
+            {
+                Value = (double)Math.Round(TotalInterest, 2),
+                Label = "Interest",
+                Color = "rgba(220, 0, 0, 0.5)"
+            };
+            SimpleData capital = new SimpleData()
+            {
+                Value = (double)Math.Round(Amount, 2),
+                Label = "Capital",
+                Color = "rgba(0, 220, 0, 0.5)"
+            };
+            pieChart.Data.Add(interest);
+            pieChart.Data.Add(capital);
+            return pieChart;
+        }
     }
 }
