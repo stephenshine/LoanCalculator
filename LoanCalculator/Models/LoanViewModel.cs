35dc99e5f26acc927478c566cee3b2ce447bfe8b
Stephen Shine <stephen.shine88@gmail.com>
2016-12-03 10:34:33 +0000

    Refining LoanViewModel

--- LoanCalculator/Models/LoanViewModel.cs
+++ LoanCalculator/Models/LoanViewModel.cs
@@ -10,24 +10,10 @@ namespace LoanCalculator.Models
     // This class is just for displaying data on the view
     public class LoanViewModel
     {
-        // set to private because only data within objects needs to be exposed
-        private Loan Loan { get; set; }
-        private LoanChart LoanChart { get; set; }
+   
+        public decimal MonthlyRepayment { get; set; }
+        public decimal TotalInterest { get; set; }
+        public decimal TotalRepaid { get; set; }
 
-        // read only properties get information from private objects
-        public decimal Amount { get { return Loan.Amount; } }
-        //public decimal MonthlyRepayment { get { return Loan.MonthlyRepayment; } }
-        public decimal TotalInterest { get { return Loan.TotalInterest; } }
-        public decimal TotalRepaid { get { return Loan.TotalRepaid; } }
-        public List<Transaction> Transactions { get { return Loan.Transactions; } }
-        public PieChart Pie { get { return LoanChart.CreatePieChart(); } }
-
-        // constructor sets private objects
-        public LoanViewModel(Loan loan)
-        {
-            Loan = loan;
-            //loan.RepayLoan();
-            LoanChart = new LoanChart(Loan);
-        }
     }
 }
