f45082784f3d72acc3a630d341ea8fc3b77f688a
Stephen Shine <stephen.shine88@gmail.com>
2016-12-04 21:52:00 +0000

    Moving loan calculation data into ViewBag

--- LoanCalculator/Controllers/HomeController.cs
+++ LoanCalculator/Controllers/HomeController.cs
@@ -17,69 +17,67 @@ namespace LoanCalculator.Controllers
             return View(new Loan(0, 0, 0));
         }
 
-        public PartialViewResult Result(Loan model)
+        public PartialViewResult Result(Loan loan)
         {
             bool ShowResults = false;
-            LoanResults viewModel = new LoanResults();
             if (ModelState.IsValid)
             {
-                viewModel.TermInMonths = model.TermInMonths;
-                decimal MonthlyInterestRate = (model.APR / 12) / 100;
-                viewModel.MonthlyRepayment = CalculateMonthlyRepayment(model.Amount, MonthlyInterestRate, model.TermInMonths);
-                decimal Repayment = viewModel.MonthlyRepayment;
+                // viewModel.TermInMonths = model.TermInMonths;
+                decimal MonthlyInterestRate = (loan.APR / 12) / 100;
+                decimal MonthlyRepayment = CalculateMonthlyRepayment(loan.Amount, MonthlyInterestRate, loan.TermInMonths);
+                ViewBag.MonthlyRepayment = MonthlyRepayment;
+                decimal Repayment = MonthlyRepayment;
 
-                decimal OutstandingBalance = model.Amount;
+                decimal OutstandingBalance = loan.Amount;
                 decimal Interest = 0;
                 decimal TotalInterest = 0;
 
-                viewModel.OpeningBalances = new decimal[model.TermInMonths];
-                viewModel.Debits = new decimal[model.TermInMonths];
-                viewModel.Credits = new decimal[model.TermInMonths];
-                viewModel.ClosingBalances = new decimal[model.TermInMonths];
+                ViewBag.OpeningBalances = new decimal[loan.TermInMonths];
+                ViewBag.Debits = new decimal[loan.TermInMonths];
+                ViewBag.Credits = new decimal[loan.TermInMonths];
+                ViewBag.ClosingBalances = new decimal[loan.TermInMonths];
 
-                for (int i = 0; i < model.TermInMonths; i++)
+                for (int i = 0; i < loan.TermInMonths; i++)
                 {
-                    viewModel.OpeningBalances[i] = OutstandingBalance;
+                    ViewBag.OpeningBalances[i] = OutstandingBalance;
 
                     Interest = Math.Round(OutstandingBalance * MonthlyInterestRate, 2);
                     OutstandingBalance += Interest;
-                    viewModel.Debits[i] = Interest;
+                    TotalInterest += Interest;
+                    ViewBag.Debits[i] = Interest;
 
-                    if (i == (model.TermInMonths -1))
+                    if (i == (loan.TermInMonths -1))
                     {
                         Repayment = OutstandingBalance;
                     }
                     OutstandingBalance -= Repayment;
-                    viewModel.Credits[i] = Repayment;
+                    ViewBag.Credits[i] = Repayment;
 
-                    viewModel.ClosingBalances[i] = OutstandingBalance;
+                    ViewBag.ClosingBalances[i] = OutstandingBalance;
                 }
 
                 ShowResults = true;
-                viewModel.TotalInterest = viewModel.Debits.Sum();
-                viewModel.TotalRepaid = viewModel.Credits.Sum();
+                ViewBag.TotalInterest = TotalInterest;
+                ViewBag.TotalRepaid = loan.Amount + TotalInterest;
 
-                PieChart pieChart = new PieChart();
-                pieChart.Data.AddRange(new List<SimpleData>
+                ViewBag.ChartData = new List<SimpleData>
                 {
                     new SimpleData
                     {
-                        Value = (double)Math.Round(viewModel.TotalInterest, 2),
+                        Value = (double)Math.Round(TotalInterest, 2),
                         Label = "Interest",
                         Color = "rgba(220, 0, 0, 0.5)",
                     },
                     new SimpleData()
                     {
-                        Value = (double)Math.Round(model.Amount, 2),
+                        Value = (double)Math.Round(loan.Amount, 2),
                         Label = "Capital",
                         Color = "rgba(0, 220, 0, 0.5)"
-                    }
-                });
-                viewModel.pieChart = pieChart;
+                    }};
             }
             ViewBag.ShowResults = ShowResults;
 
-            return PartialView(viewModel);
+            return PartialView(loan);
         }
 
         private decimal CalculateMonthlyRepayment(decimal Amount, decimal MonthlyInterestRate, int TermInMonths)  
