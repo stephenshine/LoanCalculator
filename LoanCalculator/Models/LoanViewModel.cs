db74c4a8f4f1694e3dc614a8656abb4caef83b6a
Stephen Shine <stephen.shine88@gmail.com>
2016-12-10 07:49:10 +0000

    Adding data annotations to LoanModelView and using Label and Value for in Results View

--- LoanCalculator/Models/LoanViewModel.cs
+++ LoanCalculator/Models/LoanViewModel.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Chart.Mvc.SimpleChart;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LoanCalculator.Models
 {
+    [DisplayName("Information about loan")]
     public class LoanViewModel
     {
         public LoanViewModel(Loan loan)
@@ -14,12 +17,26 @@ namespace LoanCalculator.Models
         }
 
         public Loan Loan { get; set; }
+
+        [DisplayName("Monthly repayment")]
         public decimal MonthlyRepayment { get; set; }
+
+        [DisplayName("Total interest")]
         public decimal TotalInterest { get; set; }
+
+        [DisplayName("Total amount to repay")]
         public decimal TotalRepaid { get; set; }
+
+        [DisplayName("Opening balance")]
         public decimal[] OpeningBalances { get; set; }
+
+        [DisplayName("Debit")]
         public decimal[] Debits { get; set; }
+
+        [DisplayName("Credit")]
         public decimal[] Credits { get; set; }
+
+        [DisplayName("Closing balance")]
         public decimal[] ClosingBalances { get; set; }
         public PieChart PieChart { get; set; }
     }
